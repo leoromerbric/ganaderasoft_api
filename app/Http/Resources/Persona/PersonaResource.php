8f8bffb10907ce120564ea8c6231b2d33cce7c9d
Edwyn <Edwynjesus30@gmail.com>
2026-07-17 14:03:28 -0400

    refactor: modularizar recursos de Persona y Propietario bajo el modulo Persona

---
+++ app/Http/Resources/Persona/PersonaResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources\Persona;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PersonaResource extends JsonResource
+{
+    /**
+     * Transforma el recurso de Persona en un array.
+     *
+     * @param Request $request
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        if (!$this->resource) {
+            return null;
+        }
+
+        return [
+            'id' => $this->id,
+            'cedula' => $this->cedula,
+            'nombre' => $this->nombre,
+            'apellido' => $this->apellido,
+            'telefono' => $this->telefono,
+            'correo' => $this->correo,
+            'status' => $this->status,
+        ];
+    }
+}
---
+++ app/Http/Resources/Persona/PropietarioResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\Persona;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PropietarioResource extends JsonResource
+{
+    /**
+     * Transforma el recurso de Propietario en un array.
+     *
+     * @param Request $request
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        if (!$this->resource) {
+            return null;
+        }
+
+        return [
+            'id' => $this->id,
+            'persona' => new PersonaResource($this->whenLoaded('persona')),
+        ];
+    }
+}
