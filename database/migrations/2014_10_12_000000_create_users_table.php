b82f902f36d9773f9c892e57a8047d59525e5fb2
Edwyn <edwynjesus30@gmail.com>
2026-08-20 19:09:23 -0400

    refactor(users): consolidate foto column directly into create_users_table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('foto')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('status', ['active', 'suspended'])->default('active');
